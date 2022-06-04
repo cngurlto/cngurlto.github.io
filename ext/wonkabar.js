b6f24c6d87c121c00baa721a68e1f15543faf239
cngurlto <106412189+cngurlto@users.noreply.github.com>
2022-06-04 09:24:36 -0400

    Create wonkabar.js

---
+++ ext/wonkabar.js
@@ -0,0 +1,9 @@
+ console.log("Starting Scripts...")
+console.log("Protected by CloudFlare")
+window.onload(function((console)
+              console.warn("Function is not defined")
+))
+while true do(
+console.log("WARNING! DO NOT PASTE ANY CODE HERE, THIS IS CALLED A SELF-XSS (Self Cross-site script) this could be used to impersonate you or other users. Only paste code if you 100% Know what you are doing. If you do, we'd love to have you on our team!")
+  wait(3)
+end)
